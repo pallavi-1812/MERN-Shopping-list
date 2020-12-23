120f94d70ea77f86ee91d88abbc303a4f907b7c0
pallavi1812 <upallavi1812@gmail.com>
2020-12-23 11:10:25 +0530

    components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar />
+        <ShoppingList />
       </div>
     );
   }
